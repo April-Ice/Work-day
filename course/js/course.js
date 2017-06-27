5ee8e72adf96ea484668f887cef13960949a93de
April <850677519@qq.com>
2017-06-27 17:26:38 +0800

    Signed-off-by: template

--- course/js/course.js
+++ course/js/course.js
@@ -11,6 +11,8 @@ $(document).ready(function () {
         })
         $(".tabsCon .tabsConItem").eq(n).show().siblings().hide()
     };
+
+
     $(".Fixed .tabsTil p").click(function () {
         $(window).scrollTop($("#tabsTil").offset().top)
     });
@@ -33,5 +35,39 @@ $(document).ready(function () {
     };
     var blog = template('test', data);
     document.getElementById('content').innerHTML = blog;
+
+    var course = {
+        cover: 'assets/img/course.jpg',
+        favnum: 520,
+        title: '深度讲解Linux企业级集群实现方案【马哥linux视频课程】',
+    }
+    var courseTop = template('top-t', course);
+    document.getElementById('couse-top').innerHTML = courseTop;
+
+
+    var abouts = [{
+            img: 'assets/img/about1.jpg',
+            title: '拯救未来：名师三人行联手打造最强系统/网络视频课程专题',
+            time: '7.1-7.7',
+            activity: '折上8折',
+            count: 5641,
+        },
+        {
+            img: 'assets/img/about1.jpg',
+            title: '拯救未来：名师三人行联手打造最强系统/网络视频课程专题',
+            time: '7.1-7.7',
+            activity: '折上8折',
+            count: 5641,
+        },
+        {
+            img: 'assets/img/about1.jpg',
+            title: '拯救未来：名师三人行联手打造最强系统/网络视频课程专题',
+            time: '7.1-7.7',
+            activity: '折上8折',
+            count: 5641,
+        },
+    ]
+    var aboutCon = template('about-t', course);
+    document.getElementById('about-con').innerHTML = aboutCon;
 
 });
