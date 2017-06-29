4c464dc54cc54c88c7994be588088a7143800798
April <850677519@qq.com>
2017-06-29 14:10:33 +0800

    接口

--- course/js/course.js
+++ course/js/course.js
@@ -29,84 +29,55 @@ $(document).ready(function () {
     // --------模板数据---------
     var detail = {};
     test();
-    // this.detail = test();
+    var course = detail.course;
+    var course_video = detail.course_video;
+    var same_course = detail.same_course;
+    var comment = detail.comment;
+    var recommend = detail.recommend;
+    var buyer = detail.buyer;
+
+    // console.log(course[0].title);
     console.log(detail);
-    // console.log(test());
+
     function test() {
-        console.log("请求准备发送");
-        var a = {a};
         $.ajax({
-            type : 'GET',
+            type: 'GET',
             url: 'http://crm.cike360.com/portal/index.php?r=background/School_course_detail',
             data: {
-                course_id:1,
-                token:102
+                course_id: 1,
+                token: 102
             },
-            async : false,
+            async: false,
             success: function (data) {
                 var mya = jQuery.parseJSON(data);
-                // console.log("mya");
-                // console.log(mya);
-                this.a = mya;
-                 detail = mya;
-                // console.log("a inner");
-                // console.log(this.a);
+                // var mya = data;
+                detail = mya;
             },
         });
-        // console.log('a');
-        // console.log(a);
-        return a;
     }
 
-    var ppt_html = template('tpl-ppt', {
-        "info": "order_info.order_data",
-        "show": "hello"
-    });
-    $("#ppt_preview").html(ppt_html);
-
     var course_html = template('tpl_course', {
-        "cover": './img/course.jpg',
+        "cover": course[0].poster,
         "favnum": 520,
-        "title": '深度讲解Linux企业级集群实现方案【马哥linux视频课程】',
-        "brief": '适用人群： Linux初学者、Linux爱好者、系统管理员。',
-        "discount": '6折',
-        "acdate": '7.1-7.7',
+        // "title": '深度讲解Linux企业级集群实现方案【马哥linux视频课程】',
+        "title": course[0].title,
+        "brief": course[0].short_summary,
+        "discount": '',
+        "acdate": '',
         "lectures": 14,
         "hour": '6小时8分钟',
-        "time": '2014-07-29',
-        "countprice": 119,
+        "time": course[0].update_time,
+        "countprice": course[0].price,
     });
     $("#couse_review").html(course_html);
 
     var course2_html = template('tpl_course2', {
-        "countprice": 119,
+        "countprice": course[0].price,
     });
     $("#couse2_review").html(course2_html);
 
     var about_html = template('tpl_about', {
-        "abouts": [
-            {
-                img: './img/about1.jpg',
-                title: '拯救未来1：名师三人行联手打造最强系统/网络视频课程专题',
-                time: '7.1-7.7',
-                activity: '折上8折',
-                count: 5641,
-            },
-            {
-                img: './img/about1.jpg',
-                title: '拯救未来2：名师三人行联手打造最强系统/网络视频课程专题',
-                time: '7.1-7.7',
-                activity: '折上8折',
-                count: 5643,
-            },
-            {
-                img: './img/about1.jpg',
-                title: '拯救未来3：名师三人行联手打造最强系统/网络视频课程专题',
-                time: '7.1-7.7',
-                activity: '折上8折',
-                count: 5645,
-            },
-        ],
+        "abouts": same_course
     });
     $("#about_review").html(about_html);
 
@@ -118,93 +89,7 @@ $(document).ready(function () {
     $("#introtab1_review").html(introtab1_html);
 
     var introtab2_html = template('tpl_introtab2', {
-        "catalog": [
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '[免费观看]',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '[免费观看]',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '[免费观看]',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '[免费观看]',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '[免费观看]',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '[免费观看]',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '[免费观看]',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-            {
-                title: '企业级集群方案演变、从DNS负载均衡到专业负载均衡应用',
-                free: '',
-                time: '47:00',
-                intro: '介绍了早期DNS负载均衡方式随之慢慢演变成专业的负载均衡方案，深入讲解均衡原理和相关细节，如持久链接、Session同步、健康状态检测等相关机制。 如果课程内容对你有帮助，请进行评价来支持马哥教育，你的支持是我们前进的最大动力！',
-            },
-
-        ],
+        "catalog": course_video,
     });
     $("#introtab2_review").html(introtab2_html);
 
@@ -216,50 +101,12 @@ $(document).ready(function () {
         "consistent_score": 5,
         "easy_score": 5,
         "answer_score": 5,
-        "arlist": [
-            {
-                avatar: './img/student.jpg',
-                nick_name: 'ljl603',
-                consistent_score: 5,
-                easy_score: 5,
-                answer_score: 5,
-                time: '2017-06-16 21:00',
-                about_review: '讲解思路清晰，购买得很有价值',
-            },
-            {
-                avatar: './img/student.jpg',
-                nick_name: 'tianchaoinn',
-                consistent_score: 5,
-                easy_score: 5,
-                answer_score: 5,
-                time: '2017-06-16 21:00',
-                about_review: '对集群的讲解非常详细，通俗易懂，是马哥课程的精髓之一。',
-            },
-            {
-                avatar: './img/student.jpg',
-                nick_name: '张_宝龙',
-                consistent_score: 5,
-                easy_score: 5,
-                answer_score: 5,
-                time: '2017-06-16 21:00',
-                about_review: '老师讲的特别好，非常实用',
-            },
-            {
-                avatar: './img/student.jpg',
-                nick_name: 'gzy3836',
-                consistent_score: 5,
-                easy_score: 5,
-                answer_score: 5,
-                time: '2017-06-16 21:00',
-                about_review: '之其然更只其所以然，顶马哥',
-            },
-        ]
+        "arlist": comment,
     });
     $("#introtab4_review").html(introtab4_html);
 
     var interest_html = template('tpl_interest', {
-        "interest_list": [
-            {
+        "interest_list": [{
                 cover: './img/like1.jpg',
                 title: '2-老男孩Linux高薪运维集群实战',
             },
@@ -291,89 +138,26 @@ $(document).ready(function () {
             name: '51CTO学院480612985(Linux官方2群)',
         },
         "students": {
-            number: 30334,
+            number: buyer,
         },
-        "studentlist": [
-            {
-                avatar: './img/student.jpg',
-                name: 'onlyjyf',
-                vip: ''
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'bobo87',
-                vip: 1
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'qq404730028',
-                vip: ''
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'onlyjyf',
-                vip: ''
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'onlyjyf',
-                vip: 1
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'qq404730028',
-                vip: ''
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'sickles',
-                vip: ''
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'onlyjyf',
-                vip: ''
-            },
-            {
-                avatar: './img/student.jpg',
-                name: '2914712111',
-                vip: 1
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'xbw123',
-                vip: ''
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'jinchuan_127',
-                vip: ''
-            },
-            {
-                avatar: './img/student.jpg',
-                name: 'bobo87',
-                vip: ''
-            },
-        ],
-        "books": [
-            {
-                cover: './img/book1.gif',
-                title: '跟老男孩学Linux运维：Web集群实战',
-                author: '老男孩',
-                public: '',
-                price: 99
-            },
-            {
-                cover: './img/book2.gif',
-                title: 'Windows内核设计思想',
-                author: '陈树宝',
-                public: '博文视点出版社',
-                price: 108
-            },
+        "studentlist": buyer,
+        // "books": [{
+        //         cover: './img/book1.gif',
+        //         title: '跟老男孩学Linux运维：Web集群实战',
+        //         author: '老男孩',
+        //         public: '',
+        //         price: 99
+        //     },
+        //     {
+        //         cover: './img/book2.gif',
+        //         title: 'Windows内核设计思想',
+        //         author: '陈树宝',
+        //         public: '博文视点出版社',
+        //         price: 108
+        //     },
 
-        ]
+        // ]
     });
     $("#teacher1_review").html(teacher1_html);
 
 });
-
