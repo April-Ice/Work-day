ed0838ed39481a2c409b7269dbdea9793a827283
April <feiyueliushang@163.com>
2018-02-09 11:14:53 +0800

    april

--- XSH/src/pages/login/login.ts
+++ XSH/src/pages/login/login.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-// import { LoginPage } from '../index';
-
-
 @Component({
 	selector: 'page-home',
 	templateUrl: 'home.html',
 	// providers: [indexService]
 })
-export class HomePage {
+export class LoginPage {
 	login: any;
 
 	constructor(
@@ -17,9 +14,4 @@ export class HomePage {
 	) {
 		this.login = false;
 	}
-
-	public gotoLogin() {
-		// this.nav.push(LoginPage);
-	}
-
 }
