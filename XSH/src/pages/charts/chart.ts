26b4eeea154a43ef0db95dc22d09fb4ebbeec031
April <feiyueliushang@163.com>
2018-02-11 16:51:52 +0800

    暂时进行延时处理

--- XSH/src/pages/charts/chart.ts
+++ XSH/src/pages/charts/chart.ts
@@ -12,39 +12,57 @@ export class ChartPage {
 
 	public chatmemu = "sales";
 
-	public echartOption: any;
+	public chart;
 
 	constructor(
 		public navCtrl: NavController,
-		public navParams: NavParams
+		public navParams: NavParams,
 	) {
-		console.log(echarts);
+
+		console.log(this.chart);
+
 
 	}
 
-	ionViewDidEnter() {
-		const ec = echarts as any;
-		const container = document.getElementById('chart');
-		const chart = ec.init(container);
-		console.log(chart);
 
-		chart.setOption({
-			series: {
-				type: 'pie',
-				data: [{
-					name: 'A', value: 10
-				}, {
-					name: 'B', value: 20
-				}, {
-					name: 'C', value: 30
-				}]
-			}
-		});
+	ionViewDidEnter() {
+		console.log(echarts);
+		this.initchart();
+	}
+
+	initchart() {
+		let ec = echarts as any;
+		if (!document.getElementById('chart')) {
+			setTimeout(() => {
+				this.initchart();
+				console.log("为空，暂时进行延时处理，后期解决");
+			}, 300);
+		} else {
+			let container = document.getElementById('chart');
+			console.log('-----' + container);
+			this.chart = ec.init(container);
+			this.chart.setOption({
+				series: {
+					type: 'pie',
+					data: [{
+						name: 'F', value: 10
+					}, {
+						name: 'B', value: 20
+					}, {
+						name: 'C', value: 30
+					}]
+				}
+			});
+		}
+
 	}
 
 	public setmemu(name) {
 		this.chatmemu = name;
 		console.log(this.chatmemu);
+		if (name == 'sales') {
+			this.initchart();
+		}
 	}
 
 }
